f0900be422e0fa4297203c5a2ebad9970873fcff
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-07-16 17:38:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,21 +11,78 @@ const video = document.getElementById("myVideo")
 const playbtn = document.getElementById("toggle");
 const playbackSpeed  = document.getElementById("playbackSpeed ");
 const volume = document.getElementById("volume");
-const buffer = document.querySelectorAll("buffer")[0];
-const progress = document.querySelectorAll("prgress_filled")[0];
-
+const progressContainer = document.querySelector(".progress_container");
+const buffer = document.querySelector(".buffer");
+const progress = document.querySelector(".progress_filled");
 const forward = document.getElementById("for25");
-const bacward = document.getElementById("back10");
-
- const handleButtonClick = ((e,type))=>{
+const backward = document.getElementById("back10");
+playbtn.innerText = "►";
+let playing = false;
+ const handleButtonClick = (e,type)=>{
     if(type==="forward")
-		console.log(video.currentTime)
+		video.currentTime+=25;
+    else if(type==="backward")
+		video.currentTime-=10;
+	 else if(type === "play")
+	 {
+		 if(playing === true){
+			 playbtn.innerText = "►"
+			 playing = false;
+			 video.pause();
+		 }
+		 else
+		 {
+			 playbtn.innerText = "❚ ❚";
+			 playing = true;
+			 video.play();
+		 }
+	 }
  }
 
-console.log(video)
-forward.onclick = (e) =>{
- handleButtonClick(e,"forward");
+ const handleBarChange = (e,type)=>{
+	 if(type==="speed")
+	 {
+		 video.playbackRate = (e.target.value)/100;
+		 console.log(video.playbackRate)
+	 }
+	 else if(type==="volume")
+	 {
+		 video.volume = e.target.value;
+	 }
+	 else if(type==="progress"){
+		 console.log("ok")
+		  video.currentTime = e.target.value;
+		
+	 }
+ }
+video.onloadedmetadata=()=>{progress.max = video.duration}
+video.ontimeupdate =()=>{
+	let currWidth = ((video.currentTime)/(video.duration))*100;
+	progress.style.width=`${currWidth}%`;
+     console.log(progress.style.width,currWidth)
 }
+video.onprogress=(e)=>{
+   let curr = (video.buffered.end(video.buffered.length-1)/video.duration)*100;
+	buffer.style.width=`${curr}%`;
+}
+
+progressContainer.addEventListener("click", (e) => {
+  const rect = progressContainer.getBoundingClientRect(); // Use the parent!
+  const clickX = e.clientX - rect.left;
+  const percent = clickX / rect.width;
+  video.currentTime = percent * video.duration;
+
+  console.log("Clicked at %:",e.target.getBoundingClientRect(), (percent * 100).toFixed(2));
+});
+
+forward.onclick = (e) =>{handleButtonClick(e,"forward")}
+backward.onclick=(e)=>{handleButtonClick(e,"backward")};
+playbtn.onclick=(e)=>{handleButtonClick(e,"play")};
+
+progress.onchange=(e)=>{handleBarChange(e,"progress")}
+volume.onchange=(e)=>{handleBarChange(e,"volume")}
+playbackSpeed.onchange=(e)=>{handleBarChange(e,"speed")}
+
 
 
 
